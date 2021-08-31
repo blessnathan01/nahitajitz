a631d79c3be737335d6a1800b91d22ea67dde98d
blessnathan01 <blessingnathaniel01@gmail.com>
2021-08-31 10:01:02 +0300

    deleted several files and added folder called component along with Products folder

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import useStyles from './styles';
+
+const Product = () => {
+
+    const classes = useStyles();
+    
+    return (
+        <div>
+            Product
+        </div>
+    )
+}
+
+export default Product;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Products = () => {
+
+    const classes = useStyles();
+
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+            </Grid>
+        </main>
+    )
+}
+
+export default Products;
